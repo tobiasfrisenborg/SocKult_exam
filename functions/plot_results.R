edd5cb0813a81aa4593566f68748621f870c1885
tobiasfrisenborg <tobias.frisenborg@gmail.com>
2020-05-17 16:29:34 +0200

    final adjustments

--- functions/plot_results.R
+++ functions/plot_results.R
@@ -23,11 +23,12 @@ plot_results <- function(baseline, equality, reputation, confirmation) {
   
   # make conditions pretty
   data$condition <- tools::toTitleCase(data$condition)
-  data$condition <- as.factor(data$condition)
-  data$condition <- ordered(data$condition, levels = c("Baseline",
-                                                       "Equality",
-                                                       "Reputation",
-                                                       "Confirmation"))
+  data_summary$condition <- tools::toTitleCase(data_summary$condition)
+  #data$condition <- as.factor(data$condition)
+  #data$condition <- ordered(data$condition, levels = c("Baseline",
+  #                                                     "Equality",
+  #                                                     "Reputation",
+  #                                                     "Confirmation"))
   
   
   
@@ -37,7 +38,7 @@ plot_results <- function(baseline, equality, reputation, confirmation) {
   difficulty_plot <- ggplot(data_summary, aes(x = difficulty, y = mean_group_answer, color = condition)) +
     geom_smooth(se = FALSE) + 
     theme_minimal() +
-    scale_color_manual(values=c("#eb5146", "#3b44ed", "#981dd1", "#23de8d")) +
+    scale_color_manual(values=c("#eb5146", "#21b9ff", "#bc21ff", "#23de8d")) +
     labs(title = 'Mean Performance over difficulty',
          x = 'Trial Difficulty',
          y = 'Mean Group Performance')
@@ -46,28 +47,28 @@ plot_results <- function(baseline, equality, reputation, confirmation) {
   cumulative_trial_plot <- ggplot(data, aes(x = trial_n, y = cum_performance, color = condition)) +
     geom_smooth(method = 'loess', se = FALSE) + 
     theme_minimal() +
-    scale_color_manual(values=c("#eb5146", "#3b44ed", "#981dd1", "#23de8d")) +
+    scale_color_manual(values=c("#eb5146", "#21b9ff", "#bc21ff", "#23de8d")) +
     labs(title = 'Cumulative Performance over Trials',
          x = 'Number of Trials',
          y = 'Mean Group Performance')
   
   # plot the effect of knowledge spread over group performance
   knowledge_spread_plot <- ggplot(data, aes(x = knowledge_sd, y = group_performance, color = condition)) +
-    geom_smooth(method = 'loess', se = FALSE, size = 1.3) + 
-    scale_color_manual(values=c("#eb5146", "#3b44ed", "#981dd1", "#23de8d")) +
+    geom_smooth(method = 'loess', se = TRUE, size = 1.3) + 
+    scale_color_manual(values=c("#eb5146", "#21b9ff", "#bc21ff", "#23de8d")) +
     theme_minimal() +
-    labs(title = 'Group Performance over Spread of Knowledge (SD)',
-         x = "Group Knowledge SD",
+    labs(title = 'Group Performance over Spread of Intelligence (SD)',
+         x = "SD of Group Intelligence",
          y = "Mean Group Performance")
   
   # plot ridgeline
   ridgeline_performance_plot <- ggplot(data, aes(x = group_performance, y = condition, fill = condition)) +
     geom_density_ridges(rel_min_height = 0.005, scale = 1.5, alpha=.95) +
-    scale_fill_cyclical(values = c("#eb5146", "#3b44ed", "#981dd1", "#23de8d")) +
+    scale_color_manual(values=c("#eb5146", "#21b9ff", "#bc21ff", "#23de8d")) +
     theme_ridges() +
     theme(text = element_text(size = 10), legend.position = "none") +
     labs(title = "Group Performance Ridgeline Plot",
-         x = "Mean Group Performance",
+         x = "Group Performance",
          y = "")
   
   
